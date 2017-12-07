c5f47ef27576e3fcdadab9c740832a89d88134f6
slimslenderslacks <slimslenderslacks@gmail.com>
2017-12-07 13:22:54 -0800

    Version 0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject automation-api-clj "0.1.8-SNAPSHOT"
+(defproject automation-api-clj "0.1.8"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
