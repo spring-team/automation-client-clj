d4125f56dcbe73773be71d6edf7683409203524d
slimslenderslacks <slimslenderslacks@gmail.com>
2017-12-08 11:24:25 -0800

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject automation-api-clj "0.2.2-SNAPSHOT"
+(defproject automation-api-clj "0.2.2"
   :description "Atomist automation client implementation in Clojure"
   :url "http://example.com/FIXM://github.com/atomisthq/automation-api-clj"
   :license {:name "Eclipse Public License"
