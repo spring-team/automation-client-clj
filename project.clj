09cb7d7418addfa73be0c2866f906d9b590f6715
Ben Griffiths <ben@atomist.com>
2017-12-12 11:26:41 +0000

    Version 0.3.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atomist/automation-api-clj "0.3.1"
+(defproject atomist/automation-api-clj "0.3.2"
   :description "Atomist automation client implementation in Clojure"
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
