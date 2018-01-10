260087896310cd7bf5804095c216273d96213031
Travis CI <james@atomist.com>
2018-01-10 12:49:03 +0000

    Rev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.atomist/automation-client-clj "0.3.9-SNAPSHOT"
+(defproject com.atomist/automation-client-clj "0.3.10-SNAPSHOT"
   :description "Atomist automation client implementation in Clojure"
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
