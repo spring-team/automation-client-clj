8afe0d6ea682f834cb21b12d4ad32422cc144527
James Carnegie <james@atomist.com>
2018-01-10 11:43:58 +0000

    lein-release plugin: preparing 0.3.6 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.atomist/automation-client-clj "0.3.6-SNAPSHOT"
+(defproject com.atomist/automation-client-clj "0.3.6"
   :description "Atomist automation client implementation in Clojure"
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
