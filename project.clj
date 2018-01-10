53111cd07fd78a1f1bcbee48ca4703f850d1b5f3
Travis CI <james@atomist.com>
2018-01-10 12:42:04 +0000

    Build semver tags too

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.atomist/automation-client-clj "0.3.8-SNAPSHOT"
+(defproject com.atomist/automation-client-clj "0.3.9-SNAPSHOT"
   :description "Atomist automation client implementation in Clojure"
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
