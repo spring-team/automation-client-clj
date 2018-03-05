c34b9765f607f7a70dc18909dc23154302e52905
kipz <me@kipz.org>
2018-03-05 22:23:18 +0000

    Update project.clj to use org.clojure/clojure 1.9.0 [atomist:generated]

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/atomisthq/automation-client-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.2.395"]
 
                  ;; websocket
